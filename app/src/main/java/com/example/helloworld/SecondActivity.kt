7dcdbd568a88a76b5f50903ec1c0b9393a2246c0
AVALON1492 <alexlozanomartin1492@gmail.com>
2026-02-13 17:26:12 +0100

    Navigation buttons added

--- app/src/main/java/com/example/helloworld/SecondActivity.kt
+++ app/src/main/java/com/example/helloworld/SecondActivity.kt
@@ -1,6 +1,8 @@
 package com.example.helloworld
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,18 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonNextA: Button = findViewById(R.id.secondButtonA)
+        buttonNextA.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonNextB: Button = findViewById(R.id.secondButtonB)
+        buttonNextB.setOnClickListener {
+            val intent = Intent(this, ThirdActivity::class.java)
+            startActivity(intent)
         }
     }
 }
