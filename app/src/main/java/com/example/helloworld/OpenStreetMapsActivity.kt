761dc17feb467e162279cfed641e46d2cefa19f0
AVALON1492 <alexlozanomartin1492@gmail.com>
2026-02-24 16:46:08 +0100

    Add marker imports and marker's location

--- app/src/main/java/com/example/helloworld/OpenStreetMapsActivity.kt
+++ app/src/main/java/com/example/helloworld/OpenStreetMapsActivity.kt
@@ -1,13 +1,39 @@
 package com.example.helloworld
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import android.graphics.drawable.BitmapDrawable
+import androidx.core.content.ContextCompat
 
 class OpenStreetMapsActivity : AppCompatActivity() {
+    val gymkhanaCoords = listOf(
+        GeoPoint(40.38779608214728, -3.627687914352839), // Tennis
+        GeoPoint(40.38788595319803, -3.627048250272035), // Futsal outdoors
+        GeoPoint(40.3887315224542, -3.628643539758645), // Fashion and design
+        GeoPoint(40.38926842612264, -3.630067893975619), // Topos
+        GeoPoint(40.38956358584258, -3.629046081389352), // Teleco
+        GeoPoint(40.38992125672989, -3.6281366497769714), // ETSISI
+        GeoPoint(40.39037466191718, -3.6270256763598447), // Library
+        GeoPoint(40.389855884803005, -3.626782180787362) // CITSEM
+    )
+
+    val gymkhanaNames = listOf(
+        "Tennis",
+        "Futsal outdoors",
+        "Fashion and design school",
+        "Topography school",
+        "Telecommunications school",
+        "ETSISI",
+        "Library",
+        "CITSEM"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Configuration.getInstance().load(this,getSharedPreferences("osmdroid", MODE_PRIVATE))
@@ -23,5 +49,26 @@ class OpenStreetMapsActivity : AppCompatActivity() {
         mapController.setZoom(18.0)
         val startPoint = GeoPoint(lat,lon)
         mapController.setCenter(startPoint)
+
+        val marker = Marker(map)
+        marker.position = startPoint
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        marker.icon = ContextCompat.getDrawable(this, android.R.drawable.ic_menu_compass) as BitmapDrawable
+        marker.title = "My current location"
+        map.overlays.add(marker)
+
+        // Add list of markers
+        addGymkhanaMarkers(map, gymkhanaCoords, gymkhanaNames, this)
+    }
+
+    fun addGymkhanaMarkers(map: MapView, coords: List<GeoPoint>, names: List<String>, context: Context) {
+        for (i in coords.indices) {
+            val marker = Marker(map)
+            marker.position = coords[i]
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            marker.icon = ContextCompat.getDrawable(context, android.R.drawable.ic_menu_compass) as BitmapDrawable
+            marker.title = names[i]
+            map.overlays.add(marker)
+        }
     }
 }
