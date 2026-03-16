ceefcf655e9028861c45e218c5de0695959fc28e
huili <huili.chen@alumnos.upm.es>
2026-03-16 20:17:48 +0100

    Add edit location

--- app/src/main/java/com/example/helloworld/PlacesActivity.kt
+++ app/src/main/java/com/example/helloworld/PlacesActivity.kt
@@ -16,9 +16,6 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.helloworld.room.AppDatabase
 import kotlinx.coroutines.launch
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 class PlacesActivity : AppCompatActivity() {
     private val TAG = "btaPlacesActivity"
@@ -26,30 +23,30 @@ class PlacesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_places)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        Log.d(TAG, "onCreate: The activity is being created.");
-
         val listView: ListView = findViewById(R.id.lvPlace)
-
         val db = AppDatabase.getDatabase(this)
 
         lifecycleScope.launch {
             val dbPlaces = db.placesDao().getAll()
-
             val roomPlaces = dbPlaces.map {
-                listOf(it.timestamp.toString(), it.latitude.toString(), it.longitude.toString(), it.altitude.toString())
+                listOf(
+                    it.name,
+                    it.type,
+                    it.latitude.toString(),
+                    it.longitude.toString(),
+                    it.altitude.toString(),
+                    it.timestamp.toString()
+                )
             }
-
-            Log.d(TAG, "Datos obtenidos de Room: $roomPlaces")
-
-            // Instanciar el adaptador con los datos de Room directamente
             val adapter = PlacesAdapter(this@PlacesActivity, roomPlaces)
-            listView.adapter = adapter  // Asignar el adaptador al ListView
+            listView.adapter = adapter
         }
     }
 
@@ -60,26 +57,30 @@ class PlacesActivity : AppCompatActivity() {
 
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
             val view = convertView ?: inflater.inflate(R.layout.listview_item_place, parent, false)
-
             val placesTextView: TextView = view.findViewById(R.id.tvPlace)
-
             val item = placesList[position]
 
-            try {
-                placesTextView.text = item[0]
-            } catch (e: Exception) {
-                Log.e("PlacesAdapter", "Error convirtiendo valores: ${e.message}")
-            }
+            placesTextView.text = item[0]
 
-            /*view.setOnClickListener {
-                val intent = Intent(context, ThirdActivity::class.java).apply {
-                    putExtra("timestamp", item[0])
-                    putExtra("latitude", item[1])
-                    putExtra("longitude", item[2])
-                    putExtra("altitude", item[3])
+            view.setOnClickListener {
+                val intent = Intent(context, OpenStreetMapsActivity::class.java).apply {
+                    putExtra("LAT", item[2].toDoubleOrNull() ?: 0.0)
+                    putExtra("LON", item[3].toDoubleOrNull() ?: 0.0)
+                    putExtra("NAME", item[0])
                 }
                 context.startActivity(intent)
-            }*/
+            }
+            view.setOnLongClickListener {
+                val intent = Intent(context, EditPlacesActivity::class.java).apply {
+                    putExtra("name", item[0])
+                    putExtra("type", item[1])
+                    putExtra("latitude", item[2])
+                    putExtra("longitude", item[3])
+                }
+                context.startActivity(intent)
+                true
+            }
+
             return view
         }
     }
